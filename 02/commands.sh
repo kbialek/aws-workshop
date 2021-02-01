58cabccba6512610ace5f75f942e3aee2c8b1a6f
Krzysztof Białek <k.bialek@fireup.pro>
2021-02-01 21:21:14 +0100

    02 - Add template-04.yaml

--- 02/commands.sh
+++ 02/commands.sh
@@ -2,17 +2,27 @@
 set -euf -o pipefail +o allexport
 
 STACK_NAME="MyEc2Stack"
+KEY_PAIR="kbialek"
 
 function deploy-stack() {
     template_file="template-$2.yaml"
     aws cloudformation deploy \
       --stack-name "$STACK_NAME" \
       --template-file "$template_file" \
-      --capabilities CAPABILITY_IAM
+      --capabilities CAPABILITY_IAM \
+      --parameter-overrides \
+        KeyPair="$KEY_PAIR"
 }
 
 function delete-stack() {
     aws cloudformation delete-stack --stack-name "$STACK_NAME"
+}
+
+function get-ec2-ip() {
+    aws cloudformation describe-stacks \
+      --stack-name "$STACK_NAME" \
+      --query "Stacks[0].Outputs[?OutputKey=='Ec2Ip'].OutputValue" \
+      --output text
 }
 
 $1 "$@"
---
+++ 02/slides/diagrams/diagram-04.py
@@ -0,0 +1,30 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.aws.compute import EC2
+from diagrams.aws.network import InternetGateway, RouteTable, VPCRouter
+from diagrams.aws.security import KMS
+from diagrams.generic.network import Firewall
+from diagrams.onprem.network import Internet
+
+graph_attr = {
+    "pad": "0",
+    "bgcolor": "transparent"
+}
+
+with Diagram("template-04", show=False, direction="LR", filename="diagram-04", graph_attr=graph_attr):
+    internet = Internet("Public Internet")
+    with Cluster("Vpc 10.0.0.0/16"):
+        internet_gateway = InternetGateway("Igw")
+        internet - internet_gateway
+        routeTable = RouteTable("RouteTable")
+        routeTable >> Edge(label="0.0.0.0/0", style="dashed") >> internet_gateway
+        with Cluster("Subnet 10.0.0.0/24"):
+            router = VPCRouter("Router\n10.0.0.1")
+            router - Edge(style="dashed") - routeTable
+            router - internet_gateway
+            ec2 = EC2("ec2\n10.0.0.x")
+            ec2 - Edge(style="dashed") - router
+            sg = Firewall("SG: 22/tcp")
+            ec2 - sg - router
+    KMS("KeyPair") - ec2
+
+
